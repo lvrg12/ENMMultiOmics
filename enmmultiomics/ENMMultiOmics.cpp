3a337bede5d269d9de7362de3424aa379537a080
Lino Virgen <lino.virgen96@gmail.com>
2017-10-09 16:02:39 -0500

    hello world commit

---
+++ enmmultiomics/ENMMultiOmics.cpp
@@ -0,0 +1,15 @@
+// ENMMultiOmics.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+
+int main()
+{
+	cout << "Hello World" << endl;
+	return 0;
+}
